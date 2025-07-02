fc3dacab603eeb9cd3b8b5d5928c773763f900f0
Samel <m72477079s@gmail.com>
2025-07-02 15:42:36 +0200

    Update chat app with username, images, video & rules

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public')); // Serve your static files from 'public' folder
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('chat message', (msg) => {
+    // msg = { user, text, type }
     io.emit('chat message', msg);
   });
 
@@ -18,7 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
