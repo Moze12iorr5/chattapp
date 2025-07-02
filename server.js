69f1fb031326887582d610bdbb912b1a211398b2
Samel <m72477079s@gmail.com>
2025-07-02 14:43:26 +0200

    Fixade port för Render deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
